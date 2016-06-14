9d9e4155e11eb1cfc1744f7309fc94724ee3fd1b
Anna Bredun <anna_bredun@mail.ru>
2016-06-14 23:26:02 +0300

    second commit

--- src/com/goit/gojavaonline/Module4/Task3/Main.java
+++ src/com/goit/gojavaonline/Module4/Task3/Main.java
@@ -4,12 +4,10 @@ package com.goit.gojavaonline.Module4.Task3;
 public class Main {
     public static void main (String[] args){
         Point a = new Point();
-
         System.out.println("Задайте координаты первой точки: ");
         a.inputPoint();
 
         Point b = new Point();
-
         System.out.println("Задайте координаты второй точки: ");
         b.inputPoint();
 
--- src/com/goit/gojavaonline/Module4/Task3/Point.java
+++ src/com/goit/gojavaonline/Module4/Task3/Point.java
@@ -8,19 +8,19 @@ public class Point {
     double x, y;
 
 
-   // void inputPoint(){
-    //    Scanner point = new Scanner(System.in);
-    //    System.out.print("Введите x: ");
-    //    double x = point.nextDouble();
-    //    System.out.print("Введите y: ");
-    //    double y = point.nextDouble();
+   void inputPoint(){
+      Scanner point = new Scanner(System.in);
+        System.out.print("Введите x: ");
+        double x = point.nextDouble();
+        System.out.print("Введите y: ");
+        double y = point.nextDouble();
 
-    //}
+    }
 
 
-    //double getDistance(a){
-    //    return Math.sqrt( Math.pow(x2-x1, 2)+ Math.pow(y2-y1, 2));
-   // }
+    double getDistance(Point a){
+       return Math.sqrt( Math.pow(x-a.x, 2)+ Math.pow(y-a.y, 2));
+   }
 
 
 }
