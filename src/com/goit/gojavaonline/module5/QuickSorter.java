dd1d54166480ae71eea0d39b89de5d38b57719e5
Anna Bredun <anna_bredun@mail.ru>
2016-06-20 22:10:52 +0300

    fourth commit

--- src/com/goit/gojavaonline/module5/QuickSorter.java
+++ src/com/goit/gojavaonline/module5/QuickSorter.java
@@ -16,7 +16,7 @@ public class QuickSorter {
     public int searchMax (int[]numbers) {
 
         int max = 0;
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 0; i > numbers.length; i++) {
             if (max > numbers[i])
                 max = numbers[i];
         }
