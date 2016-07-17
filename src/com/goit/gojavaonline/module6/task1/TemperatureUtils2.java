61e579fc786925343b4eb281c78793405b79104d
B-Ganna <anna_bredun@mail.ru>
2016-07-17 16:32:10 +0300

    fifth commit

--- src/com/goit/gojavaonline/module6/task1/TemperatureUtils2.java
+++ src/com/goit/gojavaonline/module6/task1/TemperatureUtils2.java
@@ -11,8 +11,8 @@ public class TemperatureUtils2 {
     }
 
     public static void main(String[] args) throws TemperatureLimitException {
-        double f = -990;
-        double c = -990;
+        double f = 0;
+        double c = 0;
 
         if (f < -459.67) {
             try {
